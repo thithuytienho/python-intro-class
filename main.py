819fa06b819472ed8e68acb309c21ffe555f3c4f
thithuytienho <thithuytienho@usf.edu>
2026-08-26 10:48:03 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+############################################
+# Thi Thuy Tien Ho # thithuytienho@usf.edu #
+#------------------------------------------#
+# main.py: prints "Hello, World!" and      #
+# examines behavior of data types.
+############################################
+
+""" we want this script to PRINT the string Hello, World!
+"""
+print ("Hello, World!")
+
+a = "Hello, World!"
+print (a)
+
